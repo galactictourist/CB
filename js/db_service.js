d0ed8640d21c54bab37d49e26d66c9f41ca3a801
Wayne Dela Cruz <ddwwcruz@gmail.com>
2017-04-07 00:04:19 +0800

    Start of work

---
+++ js/db_service.js
@@ -0,0 +1,48 @@
+// var DBService = angular.module('DBService', ['$q'])
+// .service('dbservice', function ($q) {
+   	
+   	 // return {
+     //        getNotes: getNotes,
+     //        addNote: addNote,
+     //        deleteNote: deleteNote
+     //    };
+
+//         function getNotes(){
+//             var deferred = $q.defer();
+
+//             Notes.find({},function(err, notes){
+//                 if (err) deferred.reject(err);
+//                 deferred.resolve(notes);
+//             });
+
+//             return deferred.promise;
+//         }
+
+//         function addNote(text){
+//             var deferred = $q.defer();
+
+//             var note = new Notes();
+//             note.text = text;
+
+//             Notes.insert(note, function(err, newNote){
+//                 if (err) deferred.reject(err);
+//                 deferred.resolve(newNote);
+//             });
+
+//             return deferred.promise;
+//         }
+
+//         function deleteNote(id){
+//             var deferred = $q.defer();
+
+//             Notes.remove({_id: id}, function(err, numRemoved){
+//                 if (err) deferred.reject(err);
+//                 deferred.resolve(numRemoved);
+//             });
+
+//             return deferred.promise;
+//         }
+
+// });
+
+ 
