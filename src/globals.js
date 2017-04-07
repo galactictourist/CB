000114a18664cfced000f53e785a15437fc52d4a
Wayne Dela Cruz <ddwwcruz@gmail.com>
2017-04-07 13:44:22 +0800

    Create global variables (used by entire app)

---
+++ src/globals.js
@@ -0,0 +1,9 @@
+import * as $ from 'jquery'
+import * as angular from 'angular'
+
+global.jQuery = global.$ = $
+global.angular = angular
+
+import 'bootstrap'
+import 'angular-route'
+import 'angular-ui-bootstrap'
