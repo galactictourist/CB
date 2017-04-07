f4cf82847cbc5803cb02810080b293f092afbb99
Wayne Dela Cruz <ddwwcruz@gmail.com>
2017-04-07 15:43:47 +0800

    Define ranch info controller in its own file

--- src/index.js
+++ src/index.js
@@ -19,26 +19,4 @@ import './ctrl/NewCowController'
 import './ctrl/NewCalveController'
 import './ctrl/NewBullController'
 import './ctrl/NewSteerController'
-
-cattleBarn.controller("RanchInfoController", function ($scope, $q, $rootScope) {
-    $scope.ranch = {};
-
-    $scope.getRanchInfo = function () {
-        var q = $q.defer();
-
-        Ranch.findOne({
-            ranchId: 'current'
-        }, function (err, doc) {
-            q.resolve(doc);
-        });
-        return q.promise;
-    };
-
-    $scope.promise = $scope.getRanchInfo();
-    $scope.promise.then(
-        function (v) {
-            $scope.ranch = v;
-            console.log(JSON.stringify($scope.ranch));
-        }
-    );
-});
+import './ctrl/RanchInfoController'
