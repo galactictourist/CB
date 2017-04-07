0d18e9da5d4f78362ad3c888c19d778da3f7000b
Wayne Dela Cruz <ddwwcruz@gmail.com>
2017-04-07 13:56:07 +0800

    Home controller defined in its own file

--- src/index.js
+++ src/index.js
@@ -84,32 +84,7 @@ cattleBarn.config(['$routeProvider', function ($routeProvider) {
     });
 }]);
 
-cattleBarn.controller("HomeController", function ($scope) {
-    $scope.flag = false;
-    $scope.dbItems = '';
-
-    Ranch.count({}, function (err, count) {
-        $scope.dbItems = count;
-        console.log('dbItems count = ' + $scope.dbItems);
-    });
-
-    //load a partial here depending on if ranch is in db already
-    $scope.getInclude = function () {
-
-        console.log('in getInclude dbItems = ' + $scope.dbItems);
-
-        if ($scope.dbItems === 0) {
-            console.log('dbItems count 2 = ' + $scope.dbItems);
-            $scope.flag = true; //check for existing ranch entry
-        }
-
-        if ($scope.flag) {
-            return "views/new-ranch.html";
-        } else {
-            return "views/home-view.html";
-        }
-    }
-});
+import './ctrl/NavCtrl'
 
 cattleBarn.controller("NewRanchController", function ($scope, $window, $location) {
     $scope.ranch = {}; // model to hold form values
