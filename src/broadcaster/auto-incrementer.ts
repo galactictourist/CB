3ab1126a9be8c30affb5a2122ff2466227061438
Wayne Dela Cruz <ddwwcruz@gmail.com>
2017-04-11 11:24:35 +0800

    Create auto incrementer

---
+++ src/broadcaster/auto-incrementer.ts
@@ -0,0 +1,13 @@
+export default class AutoIncrementer {
+    constructor(
+        protected value = 0
+    ) { }
+
+    get currentValue() {
+        return this.value
+    }
+
+    get nextValue() {
+        return ++this.value
+    }
+}
