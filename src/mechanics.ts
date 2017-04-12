964268d059734e0515caff437d762f41fa452558
Wayne Dela Cruz <ddwwcruz@gmail.com>
2017-04-12 22:12:20 +0800

    Only attempt to read file if config file exists

--- src/mechanics.ts
+++ src/mechanics.ts
@@ -8,14 +8,16 @@ import * as actions from './actions'
 import * as stores from './stores'
 
 exists('./config/app.json', configExists => {
-    readFile('./config/app.json', 'utf-8', (err, file) => {
-        if (err) {
-            console.error(err)
-        } else {
-            let value = JSON.parse(file)
-            stores.ranchInfo.value = value
-        }
-    })
+    if (configExists) {
+        readFile('./config/app.json', 'utf-8', (err, file) => {
+            if (err) {
+                console.error(err)
+            } else {
+                let value = JSON.parse(file)
+                stores.ranchInfo.value = value
+            }
+        })
+    }
 })
 
 stores.ranchInfo.subscribe(value => {
