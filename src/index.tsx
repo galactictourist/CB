67a974fc28cd005e4694ec26c068371bea19ad9a
Wayne Dela Cruz <ddwwcruz@gmail.com>
2017-04-12 22:40:31 +0800

    App component extends base component

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,14 @@ import { render } from 'react-dom'
 
 import './mechanics'
 
+import Component from './components'
 import NavBar from './components/navbar'
 
 interface P { }
 
 interface S { }
 
-class App extends React.Component<P, S>{
+class App extends Component<P, S> {
     render() {
         return <div>
             <NavBar />
