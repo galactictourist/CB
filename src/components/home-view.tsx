11900a100249843e8067269a9f82ec30918925e9
Wayne Dela Cruz <ddwwcruz@gmail.com>
2017-04-12 18:58:45 +0800

    Create home view component

---
+++ src/components/home-view.tsx
@@ -0,0 +1,83 @@
+import * as React from 'react'
+import {
+    Container,
+    Table,
+} from 'reactstrap'
+
+interface P {
+}
+
+interface S {
+}
+
+export default class HomeView extends React.Component<P, S> {
+    get head() {
+        return <thead>
+            <th>Type</th>
+            <th>Active</th>
+            <th>Inactive</th>
+            <th>Reference</th>
+        </thead>
+    }
+
+    get bulls() {
+        return <tr>
+            <td>Bulls</td>
+            <td>23</td>
+            <td>23</td>
+            <td>23</td>
+        </tr>
+    }
+
+    get calves() {
+        return <tr>
+            <td>Calves</td>
+            <td>23</td>
+            <td>23</td>
+            <td>23</td>
+        </tr>
+    }
+
+    get steers() {
+        return <tr>
+            <td>Steers</td>
+            <td>23</td>
+            <td>23</td>
+            <td>23</td>
+        </tr>
+    }
+
+    get cows() {
+        return <tr>
+            <td>Cows</td>
+            <td>23</td>
+            <td>23</td>
+            <td>23</td>
+        </tr>
+    }
+
+    get heifers() {
+        return <tr>
+            <td>Heifers</td>
+            <td>23</td>
+            <td>23</td>
+            <td>23</td>
+        </tr>
+    }
+
+    render() {
+        return <Container fluid>
+            <h1>Welcome to Cattle Barn</h1>
+            <Table bordered>
+                {this.head}
+                <tbody>
+                    {this.bulls}
+                    {this.calves}
+                    {this.steers}
+                    {this.cows}
+                    {this.heifers}
+                </tbody>
+            </Table>
+        </Container>
+    }
+}
