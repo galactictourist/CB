8546c74be8d59a25f41c69289162392c305b8246
Wayne Dela Cruz <ddwwcruz@gmail.com>
2017-04-12 19:56:13 +0800

    Bind field inputs
    Input text fields now hold their input values

--- src/components/new-ranch.tsx
+++ src/components/new-ranch.tsx
@@ -47,25 +47,47 @@ export default class NewRanch extends React.Component<P, S> {
     }
 
     render() {
+        const s = this.state
+
         return <Container fluid>
             <h2>New Ranch Register</h2>
             <Row>
                 <Col sm={4}>
                     <FormGroup>
                         <Label for='ranch-name-input'>Ranch Name</Label>
-                        <Input type='text' id='ranch-name-input' />
+                        <Input type='text' id='ranch-name-input'
+                            value={s.ranchName}
+                            onChange={event => {
+                                this.setState({
+                                    ranchName: event.target.value
+                                })
+                            }} />
                     </FormGroup>
                 </Col>
                 <Col sm={4}>
                     <FormGroup>
                         <Label for='ranch-owner-input'>Ranch Owner</Label>
-                        <Input type='text' id='ranch-owner-input' />
+                        <Input type='text' id='ranch-owner-input'
+                            value={s.ranchOwner}
+                            onChange={event => {
+                                this.setState({
+                                    ranchOwner: event.target.value
+                                })
+                            }}
+                        />
                     </FormGroup>
                 </Col>
                 <Col sm={4}>
                     <FormGroup>
                         <Label for='phone'>Phone</Label>
-                        <Input type='text' id='phone' />
+                        <Input type='text' id='phone'
+                            value={s.phone}
+                            onChange={event => {
+                                this.setState({
+                                    phone: event.target.value
+                                })
+                            }}
+                        />
                     </FormGroup>
                 </Col>
             </Row>
@@ -73,50 +95,113 @@ export default class NewRanch extends React.Component<P, S> {
                 <Col sm={5}>
                     <FormGroup>
                         <Label for='address'>Address</Label>
-                        <Input type='text' id='address' />
+                        <Input type='text' id='address'
+                            value={s.address}
+                            onChange={event => {
+                                this.setState({
+                                    address: event.target.value
+                                })
+                            }}
+                        />
                     </FormGroup>
                 </Col>
                 <Col sm={3}>
                     <FormGroup>
                         <Label for='city'>City</Label>
-                        <Input type='text' id='city' />
+                        <Input type='text' id='city'
+                            value={s.city}
+                            onChange={event => {
+                                this.setState({
+                                    city: event.target.value
+                                })
+                            }}
+                        />
                     </FormGroup>
                 </Col>
                 <Col sm={3}>
                     <FormGroup>
                         <Label for='state'>State</Label>
-                        <Input type='text' id='state' />
+                        <Input type='text' id='state'
+                            value={s.state}
+                            onChange={event => {
+                                this.setState({
+                                    state: event.target.value
+                                })
+                            }}
+                        />
                     </FormGroup>
                 </Col>
                 <Col sm={1}>
                     <FormGroup>
                         <Label for='zip'>Zip</Label>
-                        <Input type='text' id='zip' />
+                        <Input type='text' id='zip'
+                            value={s.zip}
+                            onChange={event => {
+                                this.setState({
+                                    zip: event.target.value
+                                })
+                            }}
+                        />
                     </FormGroup>
                 </Col>
             </Row>
             <Row>
                 <Col sm={4}>
                     <Label for='premises-id'>Premises ID</Label>
-                    <Input type='text' id='premises-id' />
+                    <Input type='text' id='premises-id'
+                        value={s.premisesId}
+                        onChange={event => {
+                            this.setState({
+                                premisesId: event.target.value
+                            })
+                        }}
+                    />
                 </Col>
                 <Col sm={4}>
                     <Label for='tax-exempt'>Tax Exempt #</Label>
-                    <Input type='text' id='tax-exempt' />
+                    <Input type='text' id='tax-exempt'
+                        value={s.taxExempt}
+                        onChange={event => {
+                            this.setState({
+                                taxExempt: event.target.value
+                            })
+                        }}
+                    />
                 </Col>
                 <Col sm={4}>
                     <Label for='timber'>Timber #</Label>
-                    <Input type='text' id='timber' />
+                    <Input type='text' id='timber'
+                        value={s.timber}
+                        onChange={event => {
+                            this.setState({
+                                timber: event.target.value
+                            })
+                        }}
+                    />
                 </Col>
             </Row>
             <Row>
                 <Col sm={4}>
                     <Label for='email'>Email</Label>
-                    <Input type='text' id='email' />
+                    <Input type='email' id='email'
+                        value={s.email}
+                        onChange={event => {
+                            this.setState({
+                                email: event.target.value
+                            })
+                        }}
+                    />
                 </Col>
                 <Col sm={4}>
                     <Label for='website'>Website</Label>
-                    <Input type='text' id='website' />
+                    <Input type='text' id='website'
+                        value={s.website}
+                        onChange={event => {
+                            this.setState({
+                                website: event.target.value
+                            })
+                        }}
+                    />
                 </Col>
                 <Col sm={4}>
                     <Label for='ranch-type'>Ranch Type</Label>
