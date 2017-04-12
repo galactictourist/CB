ccd14cae31730eb6cc45ee9cfb04b88139bc96b3
Wayne Dela Cruz <ddwwcruz@gmail.com>
2017-04-12 22:09:18 +0800

    Create mechanic for when config file exists

--- src/mechanics.ts
+++ src/mechanics.ts
@@ -6,3 +6,14 @@ import {
 
 import * as actions from './actions'
 import * as stores from './stores'
+
+exists('./config/app.json', configExists => {
+    readFile('./config/app.json', 'utf-8', (err, file) => {
+        if (err) {
+            console.error(err)
+        } else {
+            let value = JSON.parse(file)
+            stores.ranchInfo.value = value
+        }
+    })
+})
