b19f3d9e51a999cac1c3548394b029200306874b
Wayne Dela Cruz <ddwwcruz@gmail.com>
2017-04-14 01:57:59 +0800

    Add bull info inputs

--- src/components/add/bull.tsx
+++ src/components/add/bull.tsx
@@ -22,6 +22,24 @@ export default class AddBull extends Component<P, S> {
     render() {
         return <Container fluid>
             <h2>Add New Bull</h2>
+            <Row>
+                <Col sm={5}>
+                    <FormGroup>Name</FormGroup>
+                    <Input type='text' />
+                </Col>
+                <Col sm={3}>
+                    <FormGroup>Reg. #</FormGroup>
+                    <Input type='text' />
+                </Col>
+                <Col sm={2}>
+                    <FormGroup>D.O.B</FormGroup>
+                    <Input type='text' />
+                </Col>
+                <Col sm={2}>
+                    <FormGroup>Birth Weight</FormGroup>
+                    <Input type='text' />
+                </Col>
+            </Row>
         </Container>
     }
 }
