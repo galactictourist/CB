1559892dfa124b91f6439c6d2a5ee4a5f3c18d50
Wayne Dela Cruz <ddwwcruz@gmail.com>
2017-04-13 20:49:24 +0800

    Force update app component on ranch info update

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import { render } from 'react-dom'
 
 import './mechanics'
 
+import * as actions from './actions'
+import * as stores from './stores'
+
 import Component from './components'
 import NavBar from './components/navbar'
 
@@ -11,6 +14,10 @@ interface P { }
 interface S { }
 
 class App extends Component<P, S> {
+    componentWillMount() {
+        stores.ranchInfo.subscribe(() => { this.forceUpdate() })
+    }
+
     render() {
         return <div>
             <NavBar />
