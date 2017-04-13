133edd5d191a41618c7d132fff785d9c8bb0d307
Wayne Dela Cruz <ddwwcruz@gmail.com>
2017-04-14 03:13:40 +0800

    Refactor the DB connections

--- src/db.ts
+++ src/db.ts
@@ -3,10 +3,46 @@ import {
     open
 } from 'sqlite'
 
-var baseDB: Database = null
+type ParamType = string | number
+interface ExecType {
+    changes: number
+    lastID: number
+    sql: string
+}
 
-open('Ranch.sqlite').then(db => {
-    baseDB = db
-}).catch(err => {
-    console.error(err)
-})
+class DB {
+    private db: Database = null
+    constructor() {
+        open('Ranch.sqlite').then(db => {
+            this.db = db
+        }).catch(err => {
+            console.error(err)
+        })
+    }
+
+    async get<T>(sql: string, ...params: ParamType[]): Promise<T[]> {
+        var get = await this.db.prepare(sql, ...params)
+        var data = await get.all()
+
+        return new Promise<T[]>((resolve, reject) => {
+            resolve(data)
+        })
+    }
+
+    async exec(sql: string, ...params: ParamType[]): Promise<ExecType> {
+        var statement = await this.db.prepare(sql, ...params)
+        var exec = await statement.run()
+
+        return new Promise<ExecType>((resolve, reject) => {
+            var changes = exec.changes
+            var lastID = exec.lastID
+            var sql = exec.sql
+
+            resolve({
+                changes,
+                lastID,
+                sql
+            })
+        })
+    }
+}
