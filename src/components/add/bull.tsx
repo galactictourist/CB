7aaa3e726098268e3a17ec6c1ace066c43406c5f
Wayne Dela Cruz <ddwwcruz@gmail.com>
2017-04-20 23:32:26 +0800

    Definition of bull add to types

--- src/components/add/bull.tsx
+++ src/components/add/bull.tsx
@@ -12,6 +12,10 @@ import {
     FormText
 } from 'reactstrap'
 
+import {
+    Bull as BullInterface
+} from '../../types'
+
 import Component from '../'
 
 import InputComp from '../input-comp'
@@ -24,48 +28,7 @@ import SiblingCodeInp from '../input-comp/sibling-code'
 interface P {
 }
 
-interface S {
-    name: string
-    regNum: string
-    dateOfBirth: string
-    birthWeight: number
-    breed: string
-    color: string
-    specialMarkings: string
-    importExport: string
-    earTag: string
-    earTagLoc: string
-    brandNum: string
-    brandNumLoc: string
-    tattoo: string
-    tattooLoc: string
-    electronicId: string
-    electronicIdLoc: string
-    otherId: string
-    otherIdLoc: string
-    metalId: string
-    metalIdLoc: string
-    genetic: string
-    bloodline: string
-    siblingCode: string
-    cloned: string
-    showAnimal: boolean
-    aiBull: boolean
-    reference: boolean
-    hornStatus: string
-    origin: string
-    imagePath: string
-    pasture: string
-    pen: string
-    currBullOwner: string
-    bullSire: string
-    bullDame: string
-    comments: string
-    purchaseFrom: string
-    purchaseDate: string
-    price: number
-    breeder: string
-}
+type S = BullInterface
 
 export default class AddBull extends Component<P, S> {
     componentWillMount() {
