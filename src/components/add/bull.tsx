190b43bdb7c0a4c52d998de265295608ab9ecc58
Wayne Dela Cruz <ddwwcruz@gmail.com>
2017-04-21 14:40:11 +0800

    [Formatting]

--- src/components/add/bull.tsx
+++ src/components/add/bull.tsx
@@ -106,7 +106,6 @@ export default class AddBull extends Component<P, S> {
             s.breeder
         )
 
-
         return new Promise<number>((resolve, reject) => {
             exec.then(e => {
                 resolve(e.lastID)
