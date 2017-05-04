4ac67435779cae49ecb51f55cacd305bdbe9b92f
Wayne Dela Cruz <ddwwcruz@gmail.com>
2017-05-04 22:29:52 +0800

    Create cow interface

--- src/types.d.ts
+++ src/types.d.ts
@@ -58,3 +58,48 @@ interface Bull {
 	price: number
 	breeder: string
 }
+
+interface Cow {
+	id?: number
+	active: number
+	name: string
+	regNum: string
+	dateOfBirth: string
+	birthWeight: number
+	breed: string
+	color: string
+	specialMarkings: string
+	importExport: string
+	earTag: string
+	earTagLoc: string
+	brandNum: string
+	brandNumLoc: string
+	tattoo: string
+	tattooLoc: string
+	electronicId: string
+	electronicIdLoc: string
+	otherId: string
+	otherIdLoc: string
+	metalId: string
+	metalIdLoc: string
+	genetic: string
+	bloodline: string
+	siblingCode: string
+	cloned: string
+	showAnimal: boolean
+	aiBull: boolean
+	reference: boolean
+	hornStatus: string
+	origin: string
+	imagePath: string
+	pasture: string
+	pen: string
+	currBullOwner: string
+	bullSire: string
+	bullDame: string
+	comments: string
+	purchaseFrom: string
+	purchaseDate: string
+	price: number
+	breeder: string
+}
