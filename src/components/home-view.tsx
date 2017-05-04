60c45d8e4621b5ba9ad0e1be7b76a2eb19e04237
Wayne Dela Cruz <ddwwcruz@gmail.com>
2017-05-04 12:14:26 +0800

    Add actual states to bull displays

--- src/components/home-view.tsx
+++ src/components/home-view.tsx
@@ -13,9 +13,45 @@ interface P {
 }
 
 interface S {
+    bullActive: number
+    bullInactive: number
+    bullReference: number
+    cowActive: number
+    cowInactive: number
+    cowReference: number
+    calfActive: number
+    calfInactive: number
+    calfReference: number
+    steersActive: number
+    steersInactive: number
+    steersReference: number
+    heiferActive: number
+    heiferInactive: number
+    heiferReference: number
+
 }
 
 export default class HomeView extends Component<P, S> {
+    comonentWillMount() {
+        this.state = {
+            bullActive: 0,
+            bullInactive: 0,
+            bullReference: 0,
+            cowActive: 0,
+            cowInactive: 0,
+            cowReference: 0,
+            calfActive: 0,
+            calfInactive: 0,
+            calfReference: 0,
+            steersActive: 0,
+            steersInactive: 0,
+            steersReference: 0,
+            heiferActive: 0,
+            heiferInactive: 0,
+            heiferReference: 0,
+        }
+    }
+
     get head() {
         return <thead>
             <tr>
